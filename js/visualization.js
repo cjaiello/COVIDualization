7cc22619e3b51d21ee1a919233c351a90cb46ff7
Christina Aiello <christinajaiello@gmail.com>
2020-03-29 11:09:00 -0400

    Added different color palettes based on data set

--- js/visualization.js
+++ js/visualization.js
@@ -10,13 +10,14 @@ jQuery(document).ready( function(){
         .attr("height", height);
 
     var dropdownData = [
-        { NAME: "Spending (avg current balance)", DATA: "AVG_CURRENT_AR" },
+        { NAME: "Insured unemployment rate", DATA: "INSURED_UNEMPLOYMENT_RATE" },
         { NAME: "Change in average spending (% vs. prev. mo.)", DATA: "AVG_CURRENT_AR_DELTA_MO" },
+        { NAME: "Spending (avg current balance)", DATA: "AVG_CURRENT_AR" },
         { NAME: "Payment speed – days beyond terms (DBT)", DATA: "AVG_DBT" },
         { NAME: "Payment speed – Cortera Payment Rating (CPR)", DATA: "AVG_CPR" },
         { NAME: "Late balances (%30+)", DATA: "AVG_PCT_LATE" },
-        { NAME: "Unemployment claims – initial filings", DATA: "INITIAL_CLAIMS" },
-        { NAME: "Insured unemployment rate", DATA: "INSURED_UNEMPLOYMENT_RATE" }];
+        { NAME: "Unemployment claims – initial filings", DATA: "INITIAL_CLAIMS" }
+    ];
     
     d3.tsv("https://s3-us-west-2.amazonaws.com/vida-public/geo/us-state-names.tsv", function(error, names) {
     
@@ -39,7 +40,7 @@ jQuery(document).ready( function(){
                 .attr("class", "land")
                 .attr("d", path);
         
-            var dropDown = d3.select(".visualization")
+            var dropDown = d3.select(".dropdown")
                 .append("select")
                 .attr("class", "parameters")
                 .on("change", function() {
@@ -78,11 +79,15 @@ jQuery(document).ready( function(){
 				});
             };
 
-            getData("AVG_CURRENT_AR", svg, us, path);
+            getData("INSURED_UNEMPLOYMENT_RATE", svg, us, path);
         });
     });
 });
 
+/*
+* Puts together an AJAX request to get the data, calls the API, 
+* and if successful calls the function to set the visualization's data.
+*/
 function getData(dropDownValue, svg, us, path) {
     jQuery.ajax({
         url : getstates_ajax.ajax_url,
@@ -93,61 +98,57 @@ function getData(dropDownValue, svg, us, path) {
         },
         success : function( response ) {
             var cleanedUpResponse = response.replace("Array","").replace("\"}]0","\"}]");
-            setVisualizationData(JSON.parse(cleanedUpResponse), svg, us, path)
+            setVisualizationData(JSON.parse(cleanedUpResponse), svg, us, path, dropDownValue)
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) { 
-            // do something 
+            // TODO
         }
     });
 }
 
-function setVisualizationData(responseData, svg, us, path) {
-    var stateNamesToData = [];
-    responseData.forEach(state => stateNamesToData[state["STATE"]] = state);  
+/*
+* Based on the dropdown value, gets a particular color scheme for the map data
+*/
+function getColorScheme(dropDownValue, largestValueInDataSet) {
+    var colorMap = {
+        "AVG_CURRENT_AR_DELTA_MO": getGreenAndRedColors(largestValueInDataSet),
+        "AVG_CURRENT_AR": getGreenColors(largestValueInDataSet),
+        "AVG_DBT": getRedColors(largestValueInDataSet),
+        "AVG_CPR": getRedColors(largestValueInDataSet),
+        "AVG_PCT_LATE": getRedColors(largestValueInDataSet),
+        "INITIAL_CLAIMS": getBlueColors(largestValueInDataSet),
+        "INSURED_UNEMPLOYMENT_RATE": getBlueColors(largestValueInDataSet)
+    };
+    var colors = colorMap[dropDownValue];
+    return colors;
+}
+
+/*
+* Sets the visualization's data and color scale.
+*/
+function setVisualizationData(responseData, svg, us, path, dropDownValue) {
+    var stateCodesToData = [];
+    responseData.forEach(state => stateCodesToData[state["STATE"]] = state);  
     
-    var largest_value = 0;
+    var largestValueInDataSet = 0;
     var all_states = topojson.feature(us, us.objects.states).features;
     all_states.forEach(function(d){
         var stateCode = idsToCodes[d.id];
-        d.info = stateNamesToData[stateCode]
+        d.info = stateCodesToData[stateCode]
         d.name = codesToNames[stateCode]
-        if (d.info.STATE_AVG > largest_value) {
-            largest_value = d.info.STATE_AVG;
+        if (d.info.STATE_AVG > largestValueInDataSet) {
+            largestValueInDataSet = d.info.STATE_AVG;
         }
     });
     
-    //make the states
-    var color_domain = [
-        -1*largest_value,
-        -.9*largest_value,
-        -.8*largest_value,
-        -.7*largest_value,
-        -.6*largest_value,
-        -.5*largest_value,
-        -.4*largest_value,
-        -.3*largest_value,
-        -.2*largest_value, 
-        -.1*largest_value, 
-        .0*largest_value, 
-        .1*largest_value, 
-        .2*largest_value, 
-        .3*largest_value, 
-        .4*largest_value, 
-        .5*largest_value, 
-        .6*largest_value, 
-        .7*largest_value, 
-        .8*largest_value, 
-        .9*largest_value, 
-        largest_value
-    ]
+    var colorDomainAndRange = getColorScheme(dropDownValue, largestValueInDataSet);
     var color = d3.scale.threshold()
-    .domain(color_domain)
-    .range(["", "#ff1919", "#ff3232", "#ff4c4c", "#ff6666", "#ff7f7f", "#ff9999", "#ffb2b2", "#ffcccc", "#ffe5e5", "#d0d6cd", "#bdc9be", "#aabdaf", "#97b0a0", "#84a491", "#719782", "#5e8b73", "#4b7e64", "#387255", "#256546", "#125937", "#004d28"]);
-    
+    .domain(colorDomainAndRange[0])
+    .range(colorDomainAndRange[1]);
+
+
     var states = svg.selectAll(".state")
         .data(all_states)
-
-    states.exit().remove()
 
     states.enter().append("path")
         .attr("class", "state")
@@ -163,6 +164,34 @@ function setVisualizationData(responseData, svg, us, path) {
                 "stroke": "black",
                 "stroke-width": 1.5
             });
+
+            var html = "";
+  
+            html += "<div class=\"tooltip_kv\">";
+            html += "<span class=\"tooltip_key\">";
+            html += d.info.STATE;
+            html += "</span>";
+            html += " <span class=\"tooltip_value\"> ";
+            html += d.info.STATE_AVG;
+            html += "";
+            html += "</span>";
+            html += "</div>";
+            
+            jQuery("#tooltip-container").html(html);
+            jQuery("#tooltip-container").show();
+                        
+            var map_width = jQuery('.visualization')[0].getBoundingClientRect().width;
+            
+            if (d3.event.layerX < map_width / 2) {
+              d3.select("#tooltip-container")
+                .style("top", (d3.event.layerY + 15) + "px")
+                .style("left", (d3.event.layerX + 15) + "px");
+            } else {
+              var tooltip_width = jQuery("#tooltip-container").width();
+              d3.select("#tooltip-container")
+                .style("top", (d3.event.layerY + 15) + "px")
+                .style("left", (d3.event.layerX - tooltip_width - 30) + "px");
+            }
         })
         .on("mouseout", function() {
             var sel = d3.select(this);
@@ -172,6 +201,7 @@ function setVisualizationData(responseData, svg, us, path) {
                 "stroke": "white",
                 "stroke-width": 1
             });
+            jQuery("#tooltip-container").hide();
         });
 
     states.transition()
@@ -179,4 +209,98 @@ function setVisualizationData(responseData, svg, us, path) {
         .style("fill", function(d) {
             return color(d.info.STATE_AVG);
         })
+}
+
+/*
+* Helper to get color values based on largest value in data set
+*/
+function getGreenColors(largestValueInDataSet) {
+    var colorDomain = [
+        .0*largestValueInDataSet, 
+        .1*largestValueInDataSet, 
+        .2*largestValueInDataSet, 
+        .3*largestValueInDataSet, 
+        .4*largestValueInDataSet, 
+        .5*largestValueInDataSet, 
+        .6*largestValueInDataSet, 
+        .7*largestValueInDataSet, 
+        .8*largestValueInDataSet, 
+        .9*largestValueInDataSet, 
+        1*largestValueInDataSet
+    ]
+    var colorRange = ["#fcfcfc", "#d0d6cd", "#bdc9be", "#aabdaf", "#97b0a0", "#84a491", "#719782", "#5e8b73", "#4b7e64", "#387255", "#256546", "#125937", "#004d28", "#004524", "#003e20"];
+    return [colorDomain, colorRange];
+}
+
+/*
+* Helper to get color values based on largest value in data set
+*/
+function getRedColors(largestValueInDataSet) {
+    var colorDomain = [
+        .0*largestValueInDataSet, 
+        .1*largestValueInDataSet, 
+        .2*largestValueInDataSet, 
+        .3*largestValueInDataSet, 
+        .4*largestValueInDataSet, 
+        .5*largestValueInDataSet, 
+        .6*largestValueInDataSet, 
+        .7*largestValueInDataSet, 
+        .8*largestValueInDataSet, 
+        .9*largestValueInDataSet, 
+        1*largestValueInDataSet
+    ]
+    var colorRange = ["#fcfcfc", "#ffe5e5", "#ffcccc", "#ffb2b2", "#ff9999", "#ff7f7f", "#ff6666", "#ff4c4c", "#ff3232", "#ff1919", "#e51616", "#ce1313"];
+    return [colorDomain, colorRange];
+}
+
+/*
+* Helper to get color values based on largest value in data set
+*/
+function getBlueColors(largestValueInDataSet) {
+    var colorDomain = [
+        .0*largestValueInDataSet, 
+        .1*largestValueInDataSet, 
+        .2*largestValueInDataSet, 
+        .3*largestValueInDataSet, 
+        .4*largestValueInDataSet, 
+        .5*largestValueInDataSet, 
+        .6*largestValueInDataSet, 
+        .7*largestValueInDataSet, 
+        .8*largestValueInDataSet, 
+        .9*largestValueInDataSet, 
+        1*largestValueInDataSet
+    ]
+    var colorRange = ["#fcfcfc", "#ebecf3", "#d8d9e7", "#c4c6dc", "#b1b4d0", "#9ea1c5", "#8a8eb9", "#777cad", "#6369a2", "#505696", "#484d87", "#404579"];
+    return [colorDomain, colorRange];
+}
+
+/*
+* Helper to get color values based on largest value in data set
+*/
+function getGreenAndRedColors(largestValueInDataSet) {
+    var colorDomain = [
+        -1*largestValueInDataSet,
+        -.9*largestValueInDataSet,
+        -.8*largestValueInDataSet,
+        -.7*largestValueInDataSet,
+        -.6*largestValueInDataSet,
+        -.5*largestValueInDataSet,
+        -.4*largestValueInDataSet,
+        -.3*largestValueInDataSet,
+        -.2*largestValueInDataSet, 
+        -.1*largestValueInDataSet, 
+        .0*largestValueInDataSet, 
+        .1*largestValueInDataSet, 
+        .2*largestValueInDataSet, 
+        .3*largestValueInDataSet, 
+        .4*largestValueInDataSet, 
+        .5*largestValueInDataSet, 
+        .6*largestValueInDataSet, 
+        .7*largestValueInDataSet, 
+        .8*largestValueInDataSet, 
+        .9*largestValueInDataSet, 
+        1*largestValueInDataSet
+    ]
+    var colorRange = ["#fcfcfc", "#ff1919", "#ff3232", "#ff4c4c", "#ff6666", "#ff7f7f", "#ff9999", "#ffb2b2", "#ffcccc", "#ffe5e5", "#d0d6cd", "#bdc9be", "#aabdaf", "#97b0a0", "#84a491", "#719782", "#5e8b73", "#4b7e64", "#387255", "#256546", "#125937", "#004d28", "#004524"];
+    return [colorDomain, colorRange];
 }
--- visualization.php
+++ visualization.php
@@ -8,12 +8,19 @@
      * License: GPL3
      */
 
-    require(plugin_dir_path( __FILE__ ).'widget.php');
+     /*
+     * Notes for debugging:
+     * Open this file in vim (or some other editor) and change DEBUG to true:
+     * vim /local/websites/dev2-www.cortera.com/htdocs/wp-config.php
+     * I have a statement in there right now that will drop all logging info
+     * into a file. Open that file to see the logging. - Christina
+     */
 
     $readMeLater = new Visualization();
     $readMeLater->run();
 
     class Visualization {
+
         /*
         * Action hooks
         */
@@ -22,20 +29,31 @@
             add_action( 'plugins_loaded', array( $this, 'register_rml' ) );
             add_action( 'plugins_loaded', array( $this, 'enqueue_rml_scripts' ) );
             add_action( 'plugins_loaded', array( $this, 'enqueue_rml_styles' ) );
-            add_action( 'wp_ajax_get_state_data', array( $this, 'get_states' ) );
+            add_action( 'wp_ajax_nopriv_get_state_data', array( $this, 'get_states' ) );
             add_filter( 'the_content', array( $this, 'display_visualization') );
         }  
         
         /*
-        * Will only show the visualization on the post named "test"
+        * Will only show the visualization on the post named "test." 
+        * add_filter( 'the_content' is run on every page and post, and then I'm either
+        * a) returning the content plus the visualization, if the post has the desired name,
+        * or b) returning the content untouched.
+        * 
+        * If you want to switch to show this on a page instead of a post, you'll need to use the $page
+        * global variable and the page int instead of page name. See more here:
+        * https://codex.wordpress.org/Global_Variables
         */
         function display_visualization ($content) {
             global $post;
+            
+            // Replace 'test' with the name of the post you want the visualization to show on.
+            // Right now this plops the visualization at the end of the post content.
+            // If you want it to show somewhere else, instead of $content .= ' you'll want to 
+            // drop a <div> into the post with a specific classname and append this to the classname.
             if ( $post->post_name == 'test' ) {
-                // Do something to $content
-                $content .= '<div class="visualization"></div>';
+                $content .= '<div class="dropdown"></div><div class="visualization"></div><div id="tooltip-container"></div>';
             }
-            return $content .= '<div class="visualization"></div>';
+            return $content;
         }
 
         /**
@@ -69,13 +87,17 @@
         /*
         * Hit via AJAX call from JavaScript.
         * Gets state data from database, based on what was selected in the dropdown.
+        * There isn't actual input to the function, but you can hit 
+        * $_POST[KEY OF VALUE PASSED OVER HERE] in the function to get info from the POST
         */
         public function get_states() {
-            global $wpdb;
-            
             $selectedDropDownValue = $_POST['selected'];
+
+            // Table names
             $CORONA_COUNTY_WEEKLY = "CORONA_COUNTY_WEEKLY";
             $CORONA_UNEMPLOYMENT_ALL = "CORONA_UNEMPLOYMENT_ALL";
+
+            // Map telling us which table contains which column of data
             $dropDownValuesToTableNames = [
                 "AVG_CURRENT_AR" => CORONA_COUNTY_WEEKLY,
                 "AVG_CURRENT_AR_DELTA_MO" => CORONA_COUNTY_WEEKLY,
@@ -85,9 +107,16 @@
                 "INITIAL_CLAIMS" => CORONA_UNEMPLOYMENT_ALL,
                 "INSURED_UNEMPLOYMENT_RATE" => CORONA_UNEMPLOYMENT_ALL
             ];
+
+            // Getting the correct table to pull the data from
             $tableContainingSelectedDropDownValue = $dropDownValuesToTableNames[$selectedDropDownValue];
-            $result = $wpdb->get_results('SELECT STATE, AVG(' . $selectedDropDownValue . ') AS STATE_AVG FROM ' . $tableContainingSelectedDropDownValue . ' GROUP BY STATE ORDER BY STATE_AVG DESC');
-            return json_encode($result);
+
+            // Lets us hit the WordPress database
+            global $wpdb;
+            $result = $wpdb->get_results('SELECT STATE, AVG(' . $selectedDropDownValue . ') AS STATE_AVG FROM ' . $tableContainingSelectedDropDownValue . ' GROUP BY STATE ORDER BY STATE');
+            
+            // You echo instead of returning (Isn't WordPress PHP fun?)
+            echo json_encode($result);
         }
     }
 
